5ffc3d6456f4bf42a6755ee0ff50039bd5c1968b
jwoodford23 <jwoodford@stumail.fhtc.edu>
2016-08-26 09:23:33 -0500

    Changed Program.java

--- TestingGit/src/Program.java
+++ TestingGit/src/Program.java
@@ -1,9 +1,12 @@
 
 public class Program {
 
+	public Scanner in = new Scanner(Stystem.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		in.close();
 	}
 
 }
